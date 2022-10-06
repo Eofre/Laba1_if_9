4dba3af0e883ec13898fb5ee666e80ca757680e2
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-10-07 02:11:00 +0800

    update

--- Laba/Program.cs
+++ Laba/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace Laba
 {
+    public class Logic
+    {
+        public static string Num(int a, int b, int c)
+        {
+
+            int p = 0;
+            if ((a < b & b < c) || (b < a) & (a < c))
+            {
+                p = a * b;
+
+            }
+            if ((c < a & a < b) || (a < c & c < b))
+            {
+                p = a * c;
+
+            }
+            if ((c < b & b < a) || (b < c & c < a))
+            {
+                p = c * b;
+
+            }
+            string message = "Произведение двух наименьших чисел равно " + p;
+            return message;
+
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите три различных числа: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            var message = Logic.Num(a, b, c);
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
---
+++ LabaTests/LogicTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Laba;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void NumTest()
+        {
+            var a = 100;
+            var b = 5;
+            var c = 900000;
+            var message = Logic.Num(a, b, c);
+            Assert.AreEqual("Произведение двух наименьших чисел равно 500", message);
+        }
+    }
+}
